9a3d99fe8a007a9d6b39028d404ad336cfcfb071
Ayman Naif <ayman_naif@hotmail.com>
2021-04-28 03:38:20 +0300

    Just uplodaing the demo, nothing done for lab 08

--- server.js
+++ server.js
@@ -1,134 +1,69 @@
 'use strict';
-const express = require('express');
 
+//Application dependencies
+const express = require('express');
+// Load Environment Variables from the .env file
 require('dotenv').config();
 const cors = require('cors');
+const pg = require('pg');
 
-const server = express();
-const superagent = require('superagent');
 
-const PORT = process.env.PORT || 3050;
-server.use(cors());
+//Application setupppp
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 8000;
 
-server.get('/location', locationHandelr);
-server.get('/weather', weatherHandler);
-server.get('/parks', parkHandler);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
-server.get('*', generalHandler);
 
-//  Location Data ................
+// ROUTES
+app.get('/test', testHandler);
+app.get('/add', addDataHandler);
+app.get('/people',getDataHandler);
+app.get('*', notFoundHandler); //Error Handler
 
-function locationHandelr(req, res) {
+// Routes Handlers
+function testHandler(request, response) {
+  response.status(200).send('ok');
+}
 
-  let cityName = req.query.city;
+function notFoundHandler(request, response) {
+  response.status(404).send('not found !!');
+}
 
-  let key = process.env.LOCATION_KEY;
-  let locURL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
-
-  superagent.get(locURL) //send a request locatioIQ API
-    .then(geoData => {
-
-      let gData = geoData.body;
-      let locationData = new Location(cityName, gData);
-      res.send(locationData);
-
+//localhost:3010/add?first_name=roaa&last_name=AbuAleeqa
+function addDataHandler(req,res){
+  console.log(req.query);
+  let firstName = req.query.first_name;
+  let lastName = req.query.last_name;
+  //safe values
+  let SQL = `INSERT INTO people (first_name,last_name) VALUES ($1,$2) RETURNING *;`;
+  let safeValues = [firstName,lastName];
+  client.query(SQL,safeValues)
+    .then(result=>{
+      res.send(result.rows);
     })
-
-    .catch(error => {
-      console.log(error);
+    .catch(error=>{
       res.send(error);
-    });
-}
-function Location(cityName, locData) {
-  this.search_query = cityName;
-  this.formatted_query = locData[0].display_name;
-  this.latitude = locData[0].lat;
-  this.longitude = locData[0].lon;
-}
-
-
-//  weather Data ................
-function Weather(weathData) {
-
-  this.forecast = weathData.weather.description;
-  this.time = new Date(weathData.valid_date).toDateString();
-}
-
-
-
-server.get('/weather', weatherHandler);
-
-function weatherHandler(req, res) {
-  let cityName = req.query.search_query;
-
-  let key = process.env.WAEATHER_KEY;
-  let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${cityName}&key=${key}`;
-
-  superagent.get(weatherURL) //send a request locatioIQ API
-    .then((geoData) => {
-      let gData = geoData.body;
-
-
-
-      let weatherArr = gData.data.map((item) => new Weather(item));
-
-
-      res.status(200).send(weatherArr.slice(0,8));
-
     })
-
-    .catch(error => {
-      console.log(error);
-      res.send(error);
-    });
-}
-//  Parks Data ................
-
-function Parks(parkData) {
-  this.name = parkData.fullname;
-  this.address = parkData.addresses[0].city, parkData.addresses[0].line1, parkData.addresses[0].line2;
-  this.fee = parkData.entranceFees[0].cost;
-  this.description = parkData.description;
 }
 
-function parkHandler(req, res) {
-  let cityName = req.query.q;
-
-  let key = process.env.PARKS_API_KEY;
-
-
-
-  let parkURL = `https://developer.nps.gov/api/v1/parks?q=${cityName}&api_key=${key}`;
-
-  superagent.get(parkURL) //send a request locatioIQ API
-    .then((parksData) => {
-      let pData = parksData.body;
-
-      let parksArr = pData.data.map((item) => new Parks(item));
-
-
-      res.status(200).send(parksArr);
-
+//localhost:3010/people
+function getDataHandler(req,res){
+  let SQL = `SELECT * FROM people;`;
+  client.query(SQL)
+    .then(result=>{
+      res.send(result.rows);
     })
-
-    .catch(error => {
-      console.log(error);
+    .catch(error=>{
       res.send(error);
-    });
+    })
 }
 
+client.connect()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`listening on ${PORT}`)
+    );
 
-//  general pages ................
-
-function generalHandler(req, res) {
-  let errorPage = {
-    status: 500,
-    resText: 'sorry! you can not access this page',
-  };
-  res.status(500).send(errorPage);
-}
-
-
-server.listen(PORT, () => {
-  console.log('listing to the Port number: ' + PORT);
-});
+  })
